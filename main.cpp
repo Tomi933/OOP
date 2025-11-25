40410b3cc712fafec5207d3e667ddca3bb2ec1bb
Tomi933 <strashivskaoksana@gmail.com>
2025-11-25 17:23:27 +0200

    add 12

--- main.cpp
+++ main.cpp
@@ -1,29 +1,161 @@
 #include <iostream>
-#include <typeinfo>
-#include "Square.h"
-#include "Circle.h"
-#include "IsoscelesTriangle.h"
-#include "Trapezoid.h"
-
+#include <string>
+#include <exception>
+#include <Windows.h>
 using namespace std;
 
-int main() {
+class Empty {};  
 
-    Shape* shapes[4];
-    shapes[0] = new Square(5);
-    shapes[1] = new Circle(3);
-    shapes[2] = new IsoscelesTriangle(4, 5);
-    shapes[3] = new Trapezoid(3, 4, 5, 6);
+class Error       
+{
+    string message;
+public:
+    Error(string m) : message(m) {}
+    string What() const { return message; }
+};
 
-    for (int i = 0; i < 4; ++i) {
-        cout << "Shape " << i + 1 << " is of type "
-            << typeid(*shapes[i]).name() 
-            << " with perimeter = " << shapes[i]->perimeter() << endl;
+class E : public exception  
+{
+    string message;
+public:
+    E(string m) : message(m) {}
+    const char* what() const noexcept override { return message.c_str(); }
+};
+
+double Solve1(double a, double b, double c)
+{
+    if (a == 0) throw 1;             
+    if (a + b == 0) throw 2;            
+
+    double x = (-b * c) / (a + b);
+
+    if (x == -c) throw 3;            
+
+    return x;
+}
+
+double Solve2(double a, double b, double c) throw()
+{
+    if (a == 0) throw 1.5;
+    if (a + b == 0) throw 2.5;
+
+    double x = (-b * c) / (a + b);
+
+    if (x == -c) throw 'x';
+
+    return x;
+}
+
+double Solve3(double a, double b, double c) throw(int)
+{
+    if (a == 0) throw 10;
+    if (a + b == 0) throw 20;
+
+    double x = (-b * c) / (a + b);
+
+    if (x == -c) throw 30;
+
+    return x;
+}
+
+double Solve4(double a, double b, double c) throw(Empty)
+{
+    if (a == 0 || a + b == 0)
+        throw Empty();
+
+    double x = (-b * c) / (a + b);
+
+    if (x == -c)
+        throw Empty();
+
+    return x;
+}
+
+double Solve5(double a, double b, double c) throw(Error)
+{
+    if (a == 0) throw Error("a = 0: рівняння не містить x");
+    if (a + b == 0) throw Error("a + b = 0: ділення на нуль у формулі");
+
+    double x = (-b * c) / (a + b);
+
+    if (x == -c) throw Error("x = -c: отримано заборонене значення");
+
+    return x;
+}
+
+double Solve6(double a, double b, double c) throw(E)
+{
+    if (a == 0) throw E("a = 0");
+    if (a + b == 0) throw E("a + b = 0");
+
+    double x = (-b * c) / (a + b);
+
+    if (x == -c) throw E("x = -c");
+
+    return x;
+}
+
+
+void MyUnexpected()
+{
+    cout << "unexpected error! - bad_exception\n";
+    throw;
+}
+
+void MyTerminate()
+{
+    cout << "terminate called: no handler\n";
+    abort();
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    set_unexpected(MyUnexpected);
+
+    double a, b, c;
+    cout << "a = ? "; cin >> a;
+    cout << "b = ? "; cin >> b;
+    cout << "c = ? "; cin >> c;
+
+    try
+    {
+        //cout << Solve1(a,b,c) << endl;
+        //cout << Solve2(a,b,c) << endl;
+        //cout << Solve3(a,b,c) << endl;
+        //cout << Solve4(a,b,c) << endl;
+        //cout << Solve5(a,b,c) << endl;
+        cout << Solve6(a, b, c) << endl; 
     }
-
-
-    for (int i = 0; i < 4; ++i) {
-        delete shapes[i];
+    catch (int v)
+    {
+        cout << "catch(int): Solve1/Solve3 -> " << v << endl;
+    }
+    catch (double v)
+    {
+        cout << "catch(double): Solve1/Solve2 -> " << v << endl;
+    }
+    catch (char)
+    {
+        cout << "catch(char): Solve2" << endl;
+    }
+    catch (Empty)
+    {
+        cout << "catch(Empty): Solve4" << endl;
+    }
+    catch (Error e)       
+    {
+        cout << "catch(Error) -> " << e.What() << endl;
+    }
+    catch (E& e)         
+    {
+        cout << "catch(E&) -> " << e.what() << endl;
+    }
+    catch (bad_exception)
+    {
+        cout << "catch(bad_exception)" << endl;
     }
 
     return 0;
